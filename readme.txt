17de807210beaf087f3f55d1f2a5f90b00e19006
Jay Krell <jaykrell@microsoft.com>
2019-07-19 02:53:21 -0700

    readme

--- readme.txt
+++ readme.txt
@@ -1,1 +1,4 @@
 A WebAssembly implementation and experimentation platform.
+portable
+simple? Always striving for the right level of complexity -- not too simple.
+efficient? (not yet)
--- w3.cpp
+++ w3.cpp
@@ -1,4 +1,7 @@
 // A WebAssembly implementation and experimentation platform.
+// portable
+// simple? Always striving for the right level of complexity -- not too simple.
+// efficient? (not yet)
 
 #if _MSC_VER
 #pragma warning (disable:4571) // catch(...)
