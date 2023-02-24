af57f4d99569ff87c16dff1ffc9706a20d466495
Jay Krell <jay.krell@cornell.edu>
2023-02-24 00:38:33 -0800

    w3rt.cpp file header

--- w3rt.cpp
+++ w3rt.cpp
@@ -1,3 +1,8 @@
+// A WebAssembly codebase by Jay Krell
+//
+// https://webassembly.github.io/spec/core/binary/index.html
+// https://webassembly.github.io/spec/core/_download/WebAssembly.pdf
+
 #include "w3.h"
 
 #undef PopulationCount64
