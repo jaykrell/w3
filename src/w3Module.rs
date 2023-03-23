7a8729f0593293765580c7ff9ba2af4f66d2debf
Jay Krell <jay.krell@cornell.edu>
2023-03-23 06:14:00 -0700

    Rust read_module work in progress.

--- src/w3Module.rs
+++ src/w3Module.rs
@@ -3,119 +3,132 @@
 // https://webassembly.github.io/spec/core/binary/index.html
 // https://webassembly.github.io/spec/core/_download/WebAssembly.pdf
 
+extern crate libc;
 use std::io;
 use std::fmt;
 use std::io::{BufReader, Read, Error, ErrorKind};
+use std::fs::File;
 
 pub struct T {
-	reader: Option<BufReader<u8>>,
+    reader: Option<BufReader<File>>,
 }
 
 impl Default for T {
     fn default() -> T {
         T {
-			reader: None,
+            reader: None,
         }
     }
 }
 
 impl T {
     fn read_byte(&mut self) -> io::Result<u64> {
-		let mut buffer = [0; 1];
-		self.reader.as_mut().unwrap().buffer().read_exact(&mut buffer)?;
-		Ok(buffer[0] as u64)
-	}
+        let mut buffer = [0; 1];
+        self.reader.as_mut().unwrap().buffer().read_exact(&mut buffer)?;
+        Ok(buffer[0] as u64)
+    }
 
     fn read_varuint7 (&mut self) -> io::Result<u64> {
-		let result = self.read_byte ();
-		match result {
-			Ok(i) => {
-				if (i & 0x80) != 0 {
-					return Err(io::Error::new(ErrorKind::InvalidData, format!("invalid {}", i)));
-				}
-				result
-			},
-			_ => result
-		}
+        let result = self.read_byte ();
+        match result {
+            Ok(i) => {
+                if (i & 0x80) != 0 {
+                    return Err(io::Error::new(ErrorKind::InvalidData, format!("invalid {}", i)));
+                }
+                result
+            },
+            _ => result
+        }
     }
 
     fn read_varuint32 (&mut self) -> io::Result<u64> {
-		self.read_varuint64()
+        self.read_varuint64()
     }
 
     fn read_varuint64 (&mut self) -> io::Result<u64> {
-		let mut result: u64 = 0;
-		let mut shift: u32 = 0;
-		loop {
-			let byte = self.read_byte().unwrap();
-			result |= ((byte & 0x7F) as u64) << shift;
-			if (byte & 0x80) == 0 {
-				return Ok(result);
-			}
-			shift += 7;
-		}
-	}
+        let mut result: u64 = 0;
+        let mut shift: u32 = 0;
+        loop {
+            let byte = self.read_byte().unwrap();
+            result |= ((byte & 0x7F) as u64) << shift;
+            if (byte & 0x80) == 0 {
+                return Ok(result);
+            }
+            shift += 7;
+        }
+    }
 
     fn read_varint (&mut self, size: u32) -> io::Result<i64> {
-		let mut result: i64 = 0;
-		let mut shift: u32 = 0;
-		let mut byte: u64;
-		loop {
-			byte = self.read_byte().unwrap();
-			result |= ((byte & 0x7F) as i64) << shift;
-			shift += 7;
-			if (byte & 0x80) == 0 {
-				break;
-			}
-		}
+        let mut result: i64 = 0;
+        let mut shift: u32 = 0;
+        let mut byte: u64;
+        loop {
+            byte = self.read_byte().unwrap();
+            result |= ((byte & 0x7F) as i64) << shift;
+            shift += 7;
+            if (byte & 0x80) == 0 {
+                break;
+            }
+        }
 
-		// sign bit of byte is second high order bit (0x40)
-		if shift < size && (byte & 0x40) != 0 {
-			result |= !0 << shift; // sign extend
-		}
+        // sign bit of byte is second high order bit (0x40)
+        if shift < size && (byte & 0x40) != 0 {
+            result |= !0 << shift; // sign extend
+        }
 
-		Ok(result)
+        Ok(result)
     }
 
     fn read_varint32 (&mut self) -> io::Result<i64> {
-		self.read_varint(64)
+        self.read_varint(64)
     }
 
     fn read_varint64 (&mut self) -> io::Result<i64> {
-		self.read_varint(64)
+        self.read_varint(64)
     }
 
-	fn read_module (_file_name: String)
-	{
-	/*
-		mmf.read (file_name);
-		base = (uint8_t*)mmf.base;
-		file_size = mmf.file.get_file_size ();
-		end = file_size + (uint8_t*)base;
+    // array of 4 bytes to little endian u32
+    fn u32le(a: &[u8]) -> u32 {
+        let mut b: u32 = 0;
+        b |= a[3] as u32; b <<= 8;
+        b |= a[2] as u32; b <<= 8;
+        b |= a[1] as u32; b <<= 8;
+        b |= a[0] as u32;
+        b
+    }
 
-		if (file_size < 8)
-			ThrowString (StringFormat ("too small %s", file_name));
+    fn read_module (&mut self, file_path: &String) -> io::Result<()> {
+        let file = File::open(file_path)?;
+        let file_size = file.metadata()?.len();
+        if file_size < 8 {
+            return Err(io::Error::new(ErrorKind::InvalidData, format!("Wasm too small {} {}", file_path, file_size)));
+        }
+        let mut reader = io::BufReader::new(file);
+        let mut buf_magic = [0; 4];
+        let mut buf_version = [0; 4];
+        reader.buffer().read_exact(&mut buf_magic)?;
+        reader.buffer().read_exact(&mut buf_version)?;
+        let magic = T::u32le(&buf_magic);
+        let expected_magic = T::u32le(&[0, 'a' as u8, 's' as u8, 'm' as u8]);
+        let version = T::u32le(&buf_version);
+        if magic != expected_magic {
+            return Err(io::Error::new(ErrorKind::InvalidData, format!("Wasm incorrect magic: {} {}", file_path, magic)));
+        }
+        if version != 1 {
+            return Err(io::Error::new(ErrorKind::InvalidData, format!("Wasm incorrect version: {} {}", file_path, version)));
+        }
+        if file_size > 8 {
+    /*
+        uint8_t* cursor = base + 8;
+        while (cursor < end)
+            read_section (&cursor);
 
-		uintLE32& magic = (uintLE32&)*base;
-		uintLE32& version = (uintLE32&)*(base + 4);
-		printf ("magic: %X\n", (uint32_t)magic);
-		printf ("version: %X\n", (uint32_t)version);
-
-		if (memcmp (&magic,"\0asm", 4))
-			ThrowString (StringFormat ("incorrect magic: %X", (uint32_t)magic));
-
-		if (version != 1)
-			ThrowString (StringFormat ("incorrect version: %X", (uint32_t)version));
-
-		// Valid module with no sections.
-		if (file_size == 8)
-			return;
-
-		uint8_t* cursor = base + 8;
-		while (cursor < end)
-			read_section (&cursor);
-
-		Assert (cursor == end);
-	*/
-	}
+        Assert (cursor == end);
+    */
+        } else {
+            // Valid module with no sections.
+        }
+        self.reader = Some(reader);
+        Ok(())
+    }
 }
